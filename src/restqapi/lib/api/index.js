50b51ad5884feedfb48b43aefe0aaef33645ae51
tony-go <gorez.tony@gmail.com>
2021-07-05 20:36:35 +0200

    fix: disable side effect between test cases

--- src/restqapi/lib/api/index.js
+++ src/restqapi/lib/api/index.js
@@ -1,26 +1,26 @@
-const got = require('got')
-const Request = require('./request')
-const Response = require('./response')
+const got = require("got");
+const { Request } = require("./request");
+const { Response } = require("./response");
 
 module.exports = function (options) {
-  const { config } = options
-  let error
+  const { config } = options;
+  let error;
   const run = async function () {
     try {
-      const options = this.request.getOptions()
-      // console.log(options)
-      const result = await got(options)
-      this.response = new Response(result.restqa)
+      const options = this.request.getOptions();
+      const result = await got(options);
+
+      this.response = new Response(result.restqa);
     } catch (e) {
-      // console.log('--------', e)
-      if (e.response) {
-        this.response = new Response(e.response.restqa)
+      // TODO: explain why we handle the reponse here.
+      if (e.response && e.response.restqa) {
+        this.response = new Response(e.response.restqa);
       } else {
-        error = e
-        throw e
+        error = e;
+        throw e;
       }
     }
-  }
+  };
   return {
     config,
     request: new Request(config.url, config.insecure),
@@ -30,8 +30,8 @@ module.exports = function (options) {
       return {
         request: this.request.getOptions(),
         response: this.response && this.response.getResult(),
-        error: error && error.message
-      }
-    }
-  }
-}
+        error: error && error.message,
+      };
+    },
+  };
+};
--- src/restqapi/lib/api/index.test.js
+++ src/restqapi/lib/api/index.test.js
@@ -1,242 +1,228 @@
-beforeEach(() => {
-  jest.resetModules()
-})
+describe("# api - Module", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-describe('# api - Module', () => {
-  test('init', () => {
-    const Api = require('./index')
-    const options = {
-      config: {
-        url: 'http://test.com'
-      }
-    }
-    const instance = new Api(options)
+  const responseSpy = jest.spyOn(require("./response"), "Response");
+  jest.mock("got", () => jest.fn());
 
-    expect(Object.keys(instance)).toHaveLength(5)
-    expect(Object.keys(instance)).toEqual(['config', 'request', 'response', 'run', 'toJSON'])
-    expect(instance.config).toEqual({ url: 'http://test.com' })
-    expect(instance.request).toBeInstanceOf(Object)
-    expect(instance.response).toBeNull()
-    expect(instance.run).toBeInstanceOf(Function)
-  })
-
-  test('run - successfull call', async () => {
-    jest.mock('./request', () => {
-      return jest.fn().mockImplementation(() => {
-        return {
-          getOptions: jest.fn(() => {
-            return { foo: 'bar' }
-          })
-        }
-      })
-    })
-
-    const got = require('got')
-    jest.mock('got', () => {
-      return jest.fn().mockResolvedValue({
-        restqa: {
-          statusCode: 201
-        }
-      })
-    })
-
-    const Response = require('./response')
-    jest.mock('./response', () => {
-      return jest.fn().mockReturnValue({
-        status: 201
-      })
-    })
-
-    const Api = require('./index')
-    const options = {
-      config: {
-        url: 'http://test.com'
-      }
-    }
-
-    const instance = new Api(options)
-    await instance.run()
-
-    expect(got.mock.calls).toHaveLength(1)
-    expect(got.mock.calls[0][0]).toEqual({ foo: 'bar' })
-
-    expect(Response.mock.calls).toHaveLength(1)
-    expect(Response.mock.calls[0][0]).toEqual({ statusCode: 201 })
-    expect(instance.response).toEqual({ status: 201 })
-  })
-
-  test('run - successfull call But api response is not a 2XX', async () => {
-    jest.mock('./request', () => {
-      return jest.fn().mockImplementation(() => {
-        return {
-          getOptions: jest.fn(() => {
-            return { foo: 'bar' }
-          })
-        }
-      })
-    })
-
-    const got = require('got')
-    jest.mock('got', () => {
-      return jest.fn().mockRejectedValue({
-        response: {
-          restqa: {
-            statusCode: 401
-          }
-        }
-      })
-    })
-
-    const Response = require('./response')
-    jest.mock('./response', () => {
-      return jest.fn().mockReturnValue({
-        status: 401
-      })
-    })
-
-    const Api = require('./index')
-    const options = {
-      config: {
-        url: 'http://test.com'
-      }
-    }
-
-    const instance = new Api(options)
-    await instance.run()
-
-    expect(got.mock.calls).toHaveLength(1)
-    expect(got.mock.calls[0][0]).toEqual({ foo: 'bar' })
-
-    expect(Response.mock.calls).toHaveLength(1)
-    expect(Response.mock.calls[0][0]).toEqual({ statusCode: 401 })
-    expect(instance.response).toEqual({ status: 401 })
-  })
-
-  test('run - unsuccessfull call (random error)', async () => {
-    jest.mock('./request', () => {
-      return jest.fn().mockImplementation(() => {
-        return {
-          getOptions: jest.fn(() => {
-            return { foo: 'bar' }
-          })
-        }
-      })
-    })
-
-    const got = require('got')
-    jest.mock('got', () => {
-      return jest.fn().mockRejectedValue(new Error('Random error'))
-    })
-
-    const Api = require('./index')
-    const options = {
-      config: {
-        url: 'http://test.com'
-      }
-    }
-
-    const instance = new Api(options)
-    await expect(instance.run()).rejects.toThrow('Random error')
-
-    expect(got.mock.calls).toHaveLength(1)
-    expect(got.mock.calls[0][0]).toEqual({ foo: 'bar' })
-
-    expect(instance.response).toBeNull()
-  })
-
-  test('toJson', async () => {
-    jest.mock('./request', () => {
-      return jest.fn().mockImplementation(() => {
-        return {
-          getOptions: jest.fn(() => {
-            return { foo: 'bar' }
-          })
-        }
-      })
-    })
-
-    const got = require('got')
-    jest.mock('got', () => {
-      return jest.fn().mockRejectedValue({
-        response: {
-          restqa: {
-            statusCode: 401
-          }
-        }
-      })
-    })
-
-    jest.mock('./response', () => {
-      return jest.fn().mockReturnValue({
-        status: 401,
-        getResult: () => {
-          return {
-            'my-result': '123'
-          }
-        }
-      })
-    })
-
-    const Api = require('./index')
-    const options = {
-      config: {
-        url: 'http://test.com'
-      }
-    }
-
-    const instance = new Api(options)
-    await instance.run()
-    const result = instance.toJSON()
-
-    expect(got.mock.calls).toHaveLength(1)
-    expect(got.mock.calls[0][0]).toEqual({ foo: 'bar' })
-
-    expect(result).toEqual({
-      request: {
-        foo: 'bar'
+  test("init", () => {
+    const got = require("got");
+    got.mockResolvedValue({
+      restqa: {
+        headers: {
+          "content-type": "application/json",
+        },
+        statusCode: 201,
       },
+    });
+
+    const Api = require("./index");
+    const options = {
+      config: {
+        url: "http://test.com",
+      },
+    };
+    const instance = new Api(options);
+
+    const instanceKeys = Object.keys(instance);
+    expect(instanceKeys).toEqual([
+      "config",
+      "request",
+      "response",
+      "run",
+      "toJSON",
+    ]);
+    expect(instance.config).toEqual({ url: "http://test.com" });
+    expect(instance.request).toBeInstanceOf(Object);
+    expect(instance.response).toEqual(null);
+    expect(instance.run).toBeInstanceOf(Function);
+  });
+
+  test("run - successfull call", async () => {
+    const got = require("got");
+    got.mockResolvedValue({
+      restqa: {
+        headers: {
+          "content-type": "application/json",
+        },
+        statusCode: 201,
+      },
+    });
+
+    const Api = require("./index");
+    const options = {
+      config: {
+        url: "http://test.com",
+      },
+    };
+
+    const instance = new Api(options);
+    await instance.run();
+
+    expect(got).toHaveBeenCalledWith(
+      expect.objectContaining({
+        hostname: "test.com",
+        protocol: "http:",
+        pathname: "/",
+      })
+    );
+
+    expect(responseSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        statusCode: 201,
+      })
+    );
+  });
+
+  test("run - successfull call But api response is not a 2XX", async () => {
+    const got = require("got");
+    got.mockRejectedValue({
       response: {
-        'my-result': '123'
-      }
-    })
-  })
+        restqa: {
+          headers: {
+            "content-type": "application/json",
+          },
+          statusCode: 401,
+        },
+      },
+    });
 
-  test('toJson when throw Error', async () => {
-    jest.mock('./request', () => {
-      return jest.fn().mockImplementation(() => {
-        return {
-          getOptions: jest.fn(() => {
-            return { foo: 'bar' }
-          })
-        }
-      })
-    })
-
-    const got = require('got')
-    jest.mock('got', () => {
-      return jest.fn().mockRejectedValue(new Error('the error'))
-    })
-
-    const Api = require('./index')
+    const Api = require("./index");
     const options = {
       config: {
-        url: 'http://test.com'
-      }
-    }
-
-    const instance = new Api(options)
-    await expect(instance.run()).rejects.toThrow('the error')
-    const result = instance.toJSON()
-
-    expect(got.mock.calls).toHaveLength(1)
-    expect(got.mock.calls[0][0]).toEqual({ foo: 'bar' })
-
-    expect(result).toEqual({
-      request: {
-        foo: 'bar'
+        url: "http://test.com",
       },
-      response: null,
-      error: 'the error'
-    })
-  })
-})
+    };
+
+    const instance = new Api(options);
+    await instance.run();
+
+    expect(got).toHaveBeenCalledWith(
+      expect.objectContaining({
+        hostname: "test.com",
+        protocol: "http:",
+        pathname: "/",
+      })
+    );
+
+    expect(responseSpy).toHaveBeenCalledWith({
+      headers: {
+        "content-type": "application/json",
+      },
+      statusCode: 401,
+    });
+
+    expect(instance.response).toEqual(
+      expect.objectContaining({ statusCode: 401 })
+    );
+  });
+
+  test("run - unsuccessfull call (random error)", async () => {
+    const got = require("got");
+    got.mockRejectedValue(new Error("Random error"));
+
+    const Api = require("./index");
+    const options = {
+      config: {
+        url: "http://test.com",
+      },
+    };
+
+    const instance = new Api(options);
+    await expect(instance.run()).rejects.toThrow("Random error");
+
+    expect(got).toHaveBeenCalledWith(
+      expect.objectContaining({
+        hostname: "test.com",
+        protocol: "http:",
+        pathname: "/",
+      })
+    );
+
+    expect(instance.response).toEqual(null);
+  });
+
+  test("toJson", async () => {
+    const got = require("got");
+    got.mockRejectedValue({
+      response: {
+        restqa: {
+          headers: {
+            "content-type": "application/json",
+          },
+          statusCode: 401,
+        },
+      },
+    });
+
+    const Api = require("./index");
+    const options = {
+      config: {
+        url: "http://test.com",
+      },
+    };
+
+    const instance = new Api(options);
+    await instance.run();
+    const result = instance.toJSON();
+
+    expect(got).toHaveBeenCalledWith(
+      expect.objectContaining({
+        hostname: "test.com",
+        protocol: "http:",
+        pathname: "/",
+      })
+    );
+
+    expect(result.request).toMatchObject({
+      hostname: "test.com",
+      port: "",
+      protocol: "http:",
+      pathname: "/",
+      responseType: "json",
+    });
+
+    expect(result.response).toEqual({
+      headers: { "content-type": "application/json" },
+      statusCode: 401,
+    });
+  });
+
+  test("toJson when throw Error", async () => {
+    const errorMessage = "the error";
+
+    const got = require("got");
+    got.mockRejectedValue(new Error(errorMessage));
+
+    const Api = require("./index");
+    const options = {
+      config: {
+        url: "http://test.com",
+      },
+    };
+
+    const instance = new Api(options);
+    await expect(instance.run()).rejects.toThrow(errorMessage);
+    const result = instance.toJSON();
+
+    expect(got).toHaveBeenCalledWith(
+      expect.objectContaining({
+        hostname: "test.com",
+        protocol: "http:",
+        pathname: "/",
+      })
+    );
+
+    expect(result.request).toMatchObject({
+      hostname: "test.com",
+      port: "",
+      protocol: "http:",
+      pathname: "/",
+      responseType: "json",
+    });
+
+    expect(result.response).toBe(null);
+    expect(result.error).toBe(errorMessage);
+  });
+});
